e9af76a96515f1fe3e71ecc86bfb43b66eaf4b3b
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-08 00:20:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 
 def get_first_name_of_season_winner(data, season)
   # code here
-  firstName
+  firstName = ""
   data[season].each do |hash|
     hash.each do |hashKey, hashValue|
       if hashValue == "Winner"
