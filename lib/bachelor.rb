633799d31330375cca2eb8abb8976dac2318633e
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-08 01:07:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -62,7 +62,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |hash|
     hash.each do |hashKey, hashValue|
       if hashKey == "age"
-        firstName = hash["name"].split(" ").first
+        sumOfAges += hashValue
       end
     end
   end
