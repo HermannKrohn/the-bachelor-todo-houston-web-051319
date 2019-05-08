610517a91d98262ec5ff937d5cb763546fd5d82a
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-05-08 00:48:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,6 +29,17 @@ end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  hometownCount = 0 
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |hashKey, hashValue|
+        if hashValue == hometownCount
+         hometownCount += 1 
+       end
+      end
+    end
+  end
+  hometownCount
 end
 
 def get_occupation(data, hometown)
